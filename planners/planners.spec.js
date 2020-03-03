8a28c5800d99e514353f5abdeafff84a81f8b826
Miley Wright <dishaliekwright@yahoo.com>
2020-03-03 14:11:48 -0500

    testing

--- planners/planners.spec.js
+++ planners/planners.spec.js
@@ -1,30 +1,62 @@
 const server = require('../api/server');
 const request = require('supertest');
 const should = require('should');
+const db = require('../database/dbConfig');
 
+// let token;
+// beforeAll(async done => {
+//   await
+//   request(server)
+//     .post("/auth/register")
+//     .send({
+//         firstName: "test",
+//         lastName: "test",
+//         email: Date.now(),
+//         city: "test",
+//         state: "test",
+//         username: "testing",
+//         password: "test", 
+//         pricing: "test",
+//         phoneNumber: Date.now()
+//     })
+//     .end((err, response) => {
+//       token = response.body.token;
+//       console.log(`token from register`, token)
+//       done()
+//     });
+// });
 describe('plannerRoutes.js', function() {
-    var token;
-    beforeEach(function(done) {
-        request(server).post('/auth/login')
-        .send({
-            username: "miley",
-            password: "test"
-        })
-        .end(function(err, res) {
-            if (err) throw err;
-            token = {access_token: res.body.token}
-            done();
-        });
-    });
+    // var token;
+    // beforeAll(function(done) {
+    //     request(server).post('/auth/login')
+    //     .send({
+    //         username: "miley",
+    //         password: "test"
+    //     })
+    //     .end(function(err, res) {
+    //         token = res.body.token;
+    //         done();
+    //     });
+    // });
 
-    it('posts', function(done) {
-        request(server).get('/auth/user')
-        .query(token)
-        .expect(201)
+    it('Sends 400 if no token is in the header', async() => {
+        const res = await request(server)
+        .get('/auth/user')
+        // .query('Authorization', `${token}`)
+        // console.log(`this is the token`,token)
+        expect(res.status).toBe(400)
         // .end(function(err, res) {
         //     should(err).equal(null);
         //     done()
         // })
+    })
+
+    it('should return JSON', () => {
+        return request(server)
+        .get('/auth/user')
+        .then(res => {
+            expect(res.type).toBe('application/json');
+        });
     })
 })
 
