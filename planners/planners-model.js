4ea7274929f8b9f8e477961aa35747e259ea2e22
Miley Wright <dishaliekwright@yahoo.com>
2020-03-05 00:22:02 -0500

    test

--- planners/planners-model.js
+++ planners/planners-model.js
@@ -57,8 +57,10 @@ function addPost(post){
 
 function updatePost(post, id){
     return db('weddingPost')
+     .where({id})
+
     .update(post, "id")
-    .where({id})
+   
     // .then(([id]) => {
     //     return getPost(id)
     // })
