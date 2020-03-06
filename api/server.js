59d093988c4051029bc6cd8d2b68c77207bdbe49
Miley Wright <dishaliekwright@yahoo.com>
2020-03-05 23:52:35 -0500

    .

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
-// const cors = require('cors');
+const cors = require('cors');
 
 const authenticate = require('../auth/authenticate-middleware');
 const authRouter = require('../auth/authRouter.js');
