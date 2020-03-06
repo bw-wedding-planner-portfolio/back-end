26fcaad30bc5748803741ddbad402f17d3aac292
Miley Wright <dishaliekwright@yahoo.com>
2020-03-05 22:54:57 -0500

    seeing if this works

--- api/server.js
+++ api/server.js
@@ -8,15 +8,16 @@ const plannersRouter = require('../planners/plannersRoutes');
 const userRouter = require('../users/userRoutes');
 const server = express();
 
+server.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://weddingportfolio.herokuapp.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+});
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
-server.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Origin,X-Auth,X-Requested-With,Content-Type,Accept,content-type,application/json,x-auth,Access-Control-Request-Method,Access-Control-Request-Headers");
-    next();
-});
+
 
 server.use('/auth', authRouter);
 server.use('/auth/user', authenticate, plannersRouter );
