c6cd68c2ab1b54080f19edaf4fc41bb3151d037f
Miley Wright <dishaliekwright@yahoo.com>
2020-03-05 22:16:13 -0500

    testing this change

--- api/server.js
+++ api/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const helmet = require('helmet');
+const helmet = require('helmet');
 const cors = require('cors');
 
 const authenticate = require('../auth/authenticate-middleware');
@@ -8,9 +8,16 @@ const plannersRouter = require('../planners/plannersRoutes');
 const userRouter = require('../users/userRoutes');
 const server = express();
 
-// server.use(helmet());
+server.use(helmet());
 server.use(cors());
 server.use(express.json());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Origin,X-Auth,X-Requested-With,Content-Type,Accept,content-type,application/json,x-auth,Access-Control-Request-Method,Access-Control-Request-Headers");
+    next();
+});
 
 server.use('/auth', authRouter);
 server.use('/auth/user', authenticate, plannersRouter );
